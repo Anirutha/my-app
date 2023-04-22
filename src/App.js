78dbac94bb74b421b694f187fbba681a72cad2e6
Anirutha <aniruthasivakumar03@gmail.com>
2023-04-22 11:43:26 +0530

    new classes added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,54 @@ import './App.css';
 import { Profiler, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Badge from 'react-bootstrap/Badge';
-import Figure from 'react-bootstrap/Figure';
 import { Product } from './Product';
-import ProductData from './ProductData'; 
+
 
 
 function App() {
- 
+  const data = [
+    {
+      
+      productName: "Fancy Product",
+      price: 40.00,
+     
+    },
+    {
+      
+      productName: "Special Item",
+      price: 18,
+    },
+    {
+      
+      productName: "Sale Item",
+      price: 25,
+      
+    },
+    {
+      productName: "Popular Item",
+      price: 40,
+      
+    },
+    {
+      productName: "Sale Item",
+      price: 25,
+
+    },
+    {
+      productName: "Fancy Product",
+      price: 120,
+
+    },
+    {
+      productName: "Special Item",
+      price: 18,
+    },
+    {
+      productName: "Popular Item",
+      price: 40,
+    },
+  ]
+
   const [count, setCount]=useState(0)
   return (
     <div className="App">
